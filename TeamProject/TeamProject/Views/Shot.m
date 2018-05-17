1fdc5861caaaad890be8da649c6ef5c05310315a
Vender <rodionygrinovich@gmail.com>
2018-05-18 02:52:45 +0300

    fixed direction of show(READ MY COMMENT)

--- TeamProject/TeamProject/Views/Shot.m
+++ TeamProject/TeamProject/Views/Shot.m
@@ -15,11 +15,16 @@
     [self.class animateWithDuration:1.f delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
         
         CGFloat X = [myTouch locationInView:myTouch.view].x;
-        [self setCenter:CGPointMake(X, [UIScreen mainScreen].bounds.size.height - 100)];
+        [self setCenter:CGPointMake(X, -300)];
     } completion:nil];
     
-//    NSLog(@"x = %f, y = %f\n\n", [myTouch locationInView:myTouch.view].x, [myTouch locationInView:myTouch.view].y);
-//    NSLog(@"x = %f, y = %f", self.frame.origin.x, self.frame.origin.y);
+    
+    /*
+    Теперь летает в нужном направлении
+    При выстреле снаряд летит по своему ректанглу gunRectangle(снаряд виден) при вылете на другой рект(HumanRect) он летит как бы под ним (хотя по логике все ректанглы на одном view и в иерархии на одном уровне....)
+    ПРЕДЛАГАЮ сделать один ректангл для Gun и Human - тогда все будет ок(предполагаю) и Снаряд будет лететь только по своему большому ректанглу (Завтра ещё 1 предложение сделаю по поводу вычисления размеров этих rect)
+    */
+    
     
 }
 
@@ -31,7 +36,7 @@
         
         CGFloat x = [myTouch locationInView:myTouch.view].x;
         CGFloat X = x;
-        [self setCenter:CGPointMake(X, ([UIScreen mainScreen].bounds.size.height - 100))];
+        [self setCenter:CGPointMake(X, -300)];
         
         
         
