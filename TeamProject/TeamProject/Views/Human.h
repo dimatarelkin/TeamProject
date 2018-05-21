3ca19295525524e305ecdfb97a7305345fe4a443
Vender <rodionygrinovich@gmail.com>
2018-05-22 01:06:48 +0300

    added methods for animation and new properties

--- TeamProject/TeamProject/Views/Human.h
+++ TeamProject/TeamProject/Views/Human.h
@@ -7,15 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "Shot.h"
+@class Shot;
 
 @interface Human : UIView
 @property(assign, nonatomic) CGRect drawHumanSpace;
 //нужно, чтобы вызвать в методе Human вызвать метода Shot(Touches begin и запустить анимацию)
 @property(weak, nonatomic) Shot *shot;
 @property(assign, nonatomic) BOOL flagHuman;
+@property(weak, nonatomic) NSTimer *timerCheck;
 
 - (void) startShotAnimatuonWhenTouchHuman:(UITouch *) touch;
 - (void) killHumanAnimation;
-
+- (void) pattertnAnimation:(CABasicAnimation *)animation x:(CGFloat)x y:(CGFloat)y duration:(CFTimeInterval)time;
+- (void) startHumanAnimation;
+- (void) pauseLayer;
+- (void) diedHumanAnimation;
+- (void) invalidatingTimer;
 @end
